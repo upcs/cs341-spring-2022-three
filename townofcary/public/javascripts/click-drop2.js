4594875cd1f1cbd07c68b7376666d559be1b264b
ayrjanson <yrjanson24@up.edu>
2022-02-22 10:47:10 -0800

    index.html - three searchable filters incorporated click-drops.js - all correspond to individual searchable filters dropdown.css - old version of dropdown (no searching) layout.css - updates page layout search-filter.css - assigns designs based on ids (group filters to make easier) sidebar.css - controls sidebar

--- townofcary/public/javascripts/click-drop2.js
+++ townofcary/public/javascripts/click-drop2.js
@@ -1,18 +1,19 @@
-function clickDrop2() { 
-    document.getElementById("sub-incident-drop").classList.toggle("show");
-      
+function myFunction2() {
+  // Declare variables
+  var input, filter, ul, li, a, i, txtValue;
+  input = document.getElementById('myInput2');
+  filter = input.value.toUpperCase();
+  ul = document.getElementById("myUL2");
+  li = ul.getElementsByTagName('li');
+
+  // Loop through all list items, and hide those who don't match the search query
+  for (i = 0; i < li.length; i++) {
+    a = li[i].getElementsByTagName("a")[0];
+    txtValue = a.textContent || a.innerText;
+    if (txtValue.toUpperCase().indexOf(filter) > -1) {
+      li[i].style.display = "";
+    } else {
+      li[i].style.display = "none";
     }
-    
-    // Close the dropdown menu if the user clicks outside of it
-    window.onclick = function(event) {
-      if (!event.target.matches('.dropbtn')) {
-        var dropdowns = document.getElementsByClassName("dropdown-content");
-        var i;
-        for (i = 0; i < dropdowns.length; i++) {
-          var openDropdown = dropdowns[i];
-          if (openDropdown.classList.contains('show')) {
-            openDropdown.classList.remove('show');
-          }
-        }
-      }
-    }
+  }
+}
