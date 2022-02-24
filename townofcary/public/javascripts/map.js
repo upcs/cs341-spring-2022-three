4af5a2e3e0061451bbf0c1f43e7f18870df66fce
inouyeg23 <74692328+inouyeg23@users.noreply.github.com>
2022-02-23 23:09:30 -0800

    installed mapbox
    installed mapbox
    created test map

---
+++ townofcary/public/javascripts/map.js
@@ -0,0 +1,7 @@
+
+mapboxgl.accessToken = 'pk.eyJ1IjoiaW5vdXllZzIzIiwiYSI6ImNsMDBrYW91djBieTQzY285cnBzNDVtem8ifQ.gGlGhsPvneHkMe_jkTo-zg';
+
+var map = new mapboxgl.Map({
+  container: 'map',
+  style: 'mapbox://styles/mapbox/streets-v11'
+});
