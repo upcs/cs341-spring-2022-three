81e391634b010fca46aba5270adfbf001dcfecd4
inouyeg23 <74692328+inouyeg23@users.noreply.github.com>
2022-03-17 19:53:51 -0700

    added regional markers
    created geojson file

--- townofcary/public/javascripts/map.js
+++ townofcary/public/javascripts/map.js
@@ -1,7 +1,82 @@
 
 mapboxgl.accessToken = 'pk.eyJ1IjoiaW5vdXllZzIzIiwiYSI6ImNsMDBrYW91djBieTQzY285cnBzNDVtem8ifQ.gGlGhsPvneHkMe_jkTo-zg';
 
+
 var map = new mapboxgl.Map({
-  container: 'map',
-  style: 'mapbox://styles/mapbox/streets-v11'
+  container: 'map', //id of container
+  style: 'mapbox://styles/inouyeg23/cl00nbx3m00ei15oabya5ycyq',
+  center: [-78.790, 35.772], //[longitude, latitude]
+  zoom: 10
 });
+
+//make default cursor on map
+map.getCanvas().style.cursor = 'default';
+
+let hoveredStateId = null;
+
+map.on('load', () => {
+  map.addSource('townofcary', {
+    'type': 'geojson',
+    // Use a URL for the value for the `data` property.
+    'url': 'mapbox://inouyeg23.1wqkvt6q'
+  });
+  
+  map.addLayer({
+    'id': 'region_fill',
+    'type': 'fill',
+    'source': 'townofcary',
+    'layout': {},
+    'paint': {
+      'fill-color': '#627BC1',
+      'fill-opacity': ['case', ['boolean', ['feature-state', 'hover'], false], 1, 0.5]
+    }
+  });
+
+  map.addLayer({
+    'id': 'state-borders',
+    'type': 'line',
+    'source': 'townofcary',
+    'layout': {},
+    'paint': {
+      'line-color': '#627BC1',
+      'line-width': 2
+    }
+  });
+
+  map.on('mousemove', 'state-fills', (e) => {
+    if (e.features.length > 0) {
+      if (hoveredStateId !== null) {
+        map.setFeatureState(
+          { source: 'townofcary', id: hoveredStateId },
+          { hover: false }
+        );
+      }
+      hoveredStateId = e.features[0].id;
+      map.setFeatureState(
+        { source: 'townofcary', id: hoveredStateId },
+        { hover: true }
+      );
+    }
+  });
+     
+    // When the mouse leaves the state-fill layer, update the feature state of the
+    // previously hovered feature.
+    map.on('mouseleave', 'state-fills', () => {
+      if (hoveredStateId !== null) {
+        map.setFeatureState(
+          { source: 'townofcary', id: hoveredStateId },
+          { hover: false }
+        );
+      }
+      hoveredStateId = null;
+    });
+
+  // Add the searchbar to the map.
+  map.addControl(
+    new MapboxGeocoder({
+    accessToken: mapboxgl.accessToken,
+    mapboxgl: mapboxgl
+    })
+  );
+});
+
