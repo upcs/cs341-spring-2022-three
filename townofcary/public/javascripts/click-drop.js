39d6944fe953fa59a2ab8cfbacf97f6460408c51
ayrjanson <yrjanson24@up.edu>
2022-04-05 11:05:34 -0700

    Commented click drop

--- townofcary/public/javascripts/click-drop.js
+++ townofcary/public/javascripts/click-drop.js
@@ -1,14 +1,17 @@
-// ADD
-
-function myFunction() {
-  // Declare variables
+// Commented on both of these functions - Anna
+function incidentDrop() {
+  // Declaration of variables to be assigned
   var input, filter, ul, li, a, i, txtValue;
+  // input: The input as typed into the filter bar
   input = document.getElementById('myInput');
+  // filter: Turns the input into all uppercase letters
   filter = input.value.toUpperCase();
+  // ul: Grabs the container for the bulleted list with possible filter values
   ul = document.getElementById("myUL");
+  // li: Grabs the bulleted values inside of the ul container
   li = ul.getElementsByTagName('li');
 
-  // Loop through all list items, and hide those who don't match the search query
+  // For all li items
   for (i = 0; i < li.length; i++) {
     a = li[i].getElementsByTagName("a")[0];
     txtValue = a.textContent || a.innerText;
@@ -20,12 +23,16 @@ function myFunction() {
   }
 }
 
-function myFunction2() {
-  // Declare variables
+function districtDrop() {
+  // Declaration of variables to be assigned
   var input, filter, ul, li, a, i, txtValue;
+  // input: The input as typed into the filter bar
   input = document.getElementById('myInput2');
+  // filter: Turns the input into all uppercase letters
   filter = input.value.toUpperCase();
+  // ul: Grabs the container for the bulleted list with possible filter values
   ul = document.getElementById("myUL2");
+  // li: Grabs the bulleted values inside of the ul container
   li = ul.getElementsByTagName('li');
 
   // Loop through all list items, and hide those who don't match the search query
