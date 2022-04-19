4385d5461ad03c6406d5f89d568a1c0716742023
ayrjanson <yrjanson24@up.edu>
2022-04-19 10:18:36 -0700

    commit

--- townofcary/public/javascripts/click-drop.js
+++ townofcary/public/javascripts/click-drop.js
@@ -22,27 +22,3 @@ function incidentDrop() {
     }
   }
 }
-
-function districtDrop() {
-  // Declaration of variables to be assigned
-  var input, filter, ul, li, a, i, txtValue;
-  // input: The input as typed into the filter bar
-  input = document.getElementById('myInput2');
-  // filter: Turns the input into all uppercase letters
-  filter = input.value.toUpperCase();
-  // ul: Grabs the container for the bulleted list with possible filter values
-  ul = document.getElementById("myUL2");
-  // li: Grabs the bulleted values inside of the ul container
-  li = ul.getElementsByTagName('li');
-
-  // For all li items, compare to the textbox input and determine if there's a matching sequence
-  for (i = 0; i < li.length; i++) {
-    a = li[i].getElementsByTagName("a")[0];
-    txtValue = a.textContent || a.innerText;
-    if (txtValue.toUpperCase().indexOf(filter) > -1) {
-      li[i].style.display = "";
-    } else {
-      li[i].style.display = "none";
-    }
-  }
-}
